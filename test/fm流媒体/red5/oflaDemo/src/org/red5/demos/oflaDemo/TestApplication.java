dd6fbc7d16b521c30b1a768a4ac61db95dbedf8b
adobey <adobey@qq.com>
2011-02-19 05:08:12 +0000

    升级到red 5 1.0

--- test/fm流媒体/red5/oflaDemo/src/org/red5/demos/oflaDemo/TestApplication.java
+++ test/fm流媒体/red5/oflaDemo/src/org/red5/demos/oflaDemo/TestApplication.java
@@ -7,19 +7,13 @@ import java.util.Vector;
 
 import org.red5.demos.oflaDemo.vc.OnlineClients;
 import org.red5.demos.oflaDemo.vc.UUClient;
-import org.red5.logging.Red5LoggerFactory;
-import org.red5.server.adapter.ApplicationAdapter;
-import org.red5.server.api.IBandwidthConfigure;
+import org.red5.server.adapter.MultiThreadedApplicationAdapter;
+import org.red5.server.api.IClient;
 import org.red5.server.api.IConnection;
 import org.red5.server.api.IScope;
 import org.red5.server.api.stream.IServerStream;
-import org.red5.server.api.stream.IStreamCapableConnection;
-import org.red5.server.api.stream.support.SimpleConnectionBWConfig;
-import org.slf4j.Logger;
 
-public class TestApplication extends ApplicationAdapter {
-
-	private static Logger log = Red5LoggerFactory.getLogger(TestApplication.class, "oflaDemo");
+public class TestApplication extends MultiThreadedApplicationAdapter {
 
 	private IScope appScope;
 
@@ -28,10 +22,10 @@ public class TestApplication extends ApplicationAdapter {
 	private int connCount = 0;
 
 	public static final String CONN_ATTR_PARAMS = "params";
-	
+
 	public static final int PARAMS_UUID = 0;
 
-	private OnlineClients  clients = new OnlineClients();
+	private OnlineClients clients = new OnlineClients();
 
 	/**
 	 * 发布的视频列表
@@ -39,35 +33,19 @@ public class TestApplication extends ApplicationAdapter {
 	private HashMap<String, String> videoList = new HashMap<String, String>();
 
 	{
-		log.info("oflaDemo created");
+
 		System.out.println("oflaDemo created");
 	}
 
-	/** {@inheritDoc} */
-	@Override
-	public boolean appStart(IScope app) {
-		log.info("oflaDemo appStart");
-		System.out.println("oflaDemo appStart");
-		appScope = app;
-		return true;
+	public TestApplication() {
 	}
 
-	/** {@inheritDoc} */
-	@Override
-	public boolean appConnect(IConnection conn, Object[] params) {
-		log.info("oflaDemo appConnect");
+	public synchronized boolean connect(IConnection conn, IScope scope, Object params[]) {
 		System.out.println("oflaDemo appConnect");
 		conn.setAttribute(CONN_ATTR_PARAMS, params);
 		measureBandwidth(conn);
-		if (conn instanceof IStreamCapableConnection) {
-			IStreamCapableConnection streamConn = (IStreamCapableConnection) conn;
-			SimpleConnectionBWConfig bwConfig = new SimpleConnectionBWConfig();
-			bwConfig.getChannelBandwidth()[IBandwidthConfigure.OVERALL_CHANNEL] = 1024 * 1024;
-			bwConfig.getChannelInitialBurst()[IBandwidthConfigure.OVERALL_CHANNEL] = 128 * 1024;
-			streamConn.setBandwidthConfigure(bwConfig);
-		}
 
-		//添加在线链接
+		// 添加在线链接
 		if (params.length >= 1) {
 			String uuid = (String) params[PARAMS_UUID];
 			if (uuid != null && uuid.trim().length() > 0) {
@@ -75,13 +53,10 @@ public class TestApplication extends ApplicationAdapter {
 			}
 		}
 
-		return super.appConnect(conn, params);
+		return super.connect(conn, scope, params);
 	}
 
-	/** {@inheritDoc} */
-	@Override
-	public void appDisconnect(IConnection conn) {
-		log.info("oflaDemo appDisconnect");
+	public synchronized void disconnect(IConnection conn, IScope scope) {
 		System.out.println("oflaDemo appDisconnect");
 
 		Object[] params = (Object[]) conn.getAttribute(CONN_ATTR_PARAMS);
@@ -89,24 +64,41 @@ public class TestApplication extends ApplicationAdapter {
 		if (appScope == conn.getScope() && serverStream != null) {
 			serverStream.close();
 		}
-		
+
 		String id = conn.getClient().getId();
-		
+
 		String videoNamePrefix = "cid" + id;
-		
+
 		Vector<String> v = getVideoList();
-		for(int i =0;v != null && i < v.size();i++){
+		for (int i = 0; v != null && i < v.size(); i++) {
 			String videoName = v.get(i);
-			if(videoName.startsWith(videoNamePrefix)){
+			if (videoName.startsWith(videoNamePrefix)) {
 				videoList.remove(videoName);
 			}
 		}
-		
-		
-		//删除在线链接
+
+		// 删除在线链接
 		clients.removeUUClientByCID(id);
 
-		super.appDisconnect(conn);
+		super.disconnect(conn, scope);
+	}
+
+	public synchronized boolean start(IScope scope) {
+		System.out.println("oflaDemo appStart");
+		appScope = scope;
+		return super.start(scope);
+	}
+
+	public synchronized void stop(IScope scope) {
+		super.stop(scope);
+	}
+
+	public synchronized boolean join(IClient client, IScope scope) {
+		return super.join(client, scope);
+	}
+
+	public synchronized void leave(IClient client, IScope scope) {
+		super.leave(client, scope);
 	}
 
 	/**
@@ -169,16 +161,17 @@ public class TestApplication extends ApplicationAdapter {
 	public int getConnCount() {
 		return connCount++;
 	}
-	
+
 	/**
 	 * 客户端 call 方法
+	 * 
 	 * @param uuid
 	 * @return
 	 */
-	public String getId(String uuid){
+	public String getId(String uuid) {
 		UUClient client = clients.getUUClientByUUID(uuid);
 		String id = null;
-		if(client != null){
+		if (client != null) {
 			id = client.getId();
 		}
 		return id;
